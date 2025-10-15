5769fb88f08310d95d80f95c56d795efd83bb8f6
Harshada Kishorilal Rundhey <harshadarundhey@gmail.com>
2025-10-15 10:29:39 +0530

    .env

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
+// Load .env variables
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
-const PORT = 3000;
 
+// Use PORT from .env, default 3000
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send(`
-    Hey Harshada !<br>
-    Welcome to your fun Node.js server.<br>
+    Hey ${process.env.MY_NAME} !<br>
+    Your favorite color is ${process.env.FAVORITE_COLOR}.<br>
+    Your favorite food is ${process.env.FAVORITE_FOOD}.<br>
+    City: ${process.env.CITY}<br>
+    Hobby: ${process.env.HOBBY}<br>
+    Language: ${process.env.LANGUAGE}<br>
+    Skill: ${process.env.SKILL}<br>
+    Welcome to your fun Node.js server!<br>
     Today is a great day to code!<br>
     Keep learning and exploring.<br>
     Build cool projects every day.<br>
@@ -14,8 +24,14 @@ app.get('/', (req, res) => {
   `);
 });
 
-
-console.log("Hey Harshada !");
+// Console log for server side
+console.log(`Hey ${process.env.MY_NAME} !`);
+console.log(`Favorite Color: ${process.env.FAVORITE_COLOR}`);
+console.log(`Favorite Food: ${process.env.FAVORITE_FOOD}`);
+console.log(`City: ${process.env.CITY}`);
+console.log(`Hobby: ${process.env.HOBBY}`);
+console.log(`Language: ${process.env.LANGUAGE}`);
+console.log(`Skill: ${process.env.SKILL}`);
 console.log("Welcome to your fun Node.js server.");
 console.log("Today is a great day to code!");
 console.log("Keep learning and exploring.");
